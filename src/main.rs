c2e099b461e3106ca3d65576738a02aeb99af772
fireice-uk <fireice-uk@users.noreply.github.com>
2021-05-23 10:45:42 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::fmt;
 use std::error::Error;
 
-trait BaseError : Error + fmt::Display {
+trait BaseErrorFmt : fmt::Display + fmt::Debug {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "You passed one to fun_needs_zero")
     }
+}
+
+trait BaseErrorError : Error {
 }
 
 #[derive(Debug)] // Needed for fmt not to complain
 struct ErrorNumberIsOne {
     c : &'static str, // Is this correct? I want read-only const string here
 }
+
 
 impl ErrorNumberIsOne {
     fn new() -> ErrorNumberIsOne {
@@ -19,16 +23,11 @@ impl ErrorNumberIsOne {
         }
     }
 }
-impl fmt::Display for ErrorNumberIsOne {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "You passed one to fun_needs_zero")
-    }
+
+
+impl BaseErrorFmt for ErrorNumberIsOne {
 }
-
-impl Error for ErrorNumberIsOne {
-
-}
-
+impl fmt::Display for ErrorNumberIsOne {}
 
 fn main() {
     println!("Hello, world!");
